22388b4a4a42e985a3e95c1d953c07262daec204
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-06-10 16:56:42 +0900

    UPDATE: Make user models_namefix

--- students/ws/users/models.py
+++ students/ws/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    fname = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
     email = models.CharField(max_length=300)
     password = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
