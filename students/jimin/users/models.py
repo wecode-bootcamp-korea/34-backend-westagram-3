be660f13d186334b1cff942ff97b1f0f88e0b4cf
jeongjimin <wlals3968@naver.com>
2022-06-10 17:21:01 +0900

    UPDATE: User 모델링 구현 수정

--- students/jimin/users/models.py
+++ students/jimin/users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):   
-    name     = models.CharField(max_length=45)
-    email    = models.CharField(max_length=300)
-    phone    = models.CharField(max_length=300)
-    password = models.CharField(max_length=45)
+    first_name      = models.CharField(max_length=45)
+    last_name       = models.CharField(max_length=45)
+    email           = models.CharField(max_length=300,unique=True)
+    phone_number    = models.CharField(max_length=20)
+    password        = models.CharField(max_length=45)
+    
     class Meta:
         db_table='users'
