f7f0fe114c5450e4efebf3401d6198861f100589
dingwan <dingwan0331@gmail.com>
2022-06-10 17:30:54 +0900

    UPDATE: models.py

--- students/jingwan/users/models.py
+++ students/jingwan/users/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name         = models.CharField(max_length=20)
-    email        = models.CharField(max_length=100)
-    password     = models.IntegerField()
-    phone_number = models.IntegerField()
+    user_name    = models.CharField(max_length=30)
+    first_name   = models.CharField(max_length=161)
+    last_name    = models.CharField(max_length=11)
+    email        = models.CharField(max_length=254, unique=True)
+    password     = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=20, unique=True)
 
     class Meta:
         db_table = 'user_informations'
