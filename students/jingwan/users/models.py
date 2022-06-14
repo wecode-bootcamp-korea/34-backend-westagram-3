3447917b6c06324a87c32cbfd088e3c2ad184dec
dingwan <dingwan0331@gmail.com>
2022-06-14 18:45:49 +0900

    UPDATE: password.CharField(max_length=40 -> 100

--- students/jingwan/users/models.py
+++ students/jingwan/users/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     last_name    = models.CharField(max_length=11)
     email        = models.CharField(max_length=254, unique=True)
     password     = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20,unique=True)
 
     class Meta:
         db_table = 'user_informations'
