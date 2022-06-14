67ee3df561c137b0d51c951f46bbbd91d1bb46ed
jeongjimin <wlals3968@naver.com>
2022-06-14 10:48:04 +0900

    UPDATE: user.views 회원가입 구현(name 조건 추가)

--- students/jimin/users/models.py
+++ students/jimin/users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):   
-    first_name      = models.CharField(max_length=45,default='')
-    last_name       = models.CharField(max_length=45,default='')
+    first_name      = models.CharField(max_length=45)
+    last_name       = models.CharField(max_length=45)
     email           = models.CharField(max_length=300,unique=True)
-    phone_number    = models.CharField(max_length=20,default='')
-    password        = models.CharField(max_length=45,default='')
+    phone_number    = models.CharField(max_length=20)
+    password        = models.CharField(max_length=45)
     
     class Meta:
         db_table='users'
--- students/jimin/users/views.py
+++ students/jimin/users/views.py
@@ -14,31 +14,62 @@ class SignUpView(View):
              email        = data['email']
              password     = data['password']
              phone_number = data['phone_number']
+             first_name   = data['first_name']
+             last_name    = data['last_name']
              
              REGEX_EMAIL        = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$' #(소문자 a~z, 대문자 A~Z, 숫자 0~9, +-_.) + @ + .
-             REGEX_PASWWORD   = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,25}$'
+             REGEX_PASSWORD   = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,25}$'
              REGEX_PHONE_NUMBER = '\d{3}-\d{4}-\d{4}'
+             REGEX_LAST_NAME='^[a-zA-Z가-힣]+$'
+             REGEX_FIRST_NAME='^[a-zA-Z가-힣]+$'
              
              if not re.match(REGEX_EMAIL,email):
                  return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
              
-             if not re.match(REGEX_PASWWORD,password):
+             if not re.match(REGEX_PASSWORD,password):
                  return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
              
              if not re.match(REGEX_PHONE_NUMBER,phone_number):
                  return JsonResponse({'message' : 'INVALID_PHONE_NUMBER'}, status = 400)
              
              if User.objects.filter(email=email).exists():
-                 return JsonResponse({'message':'DUPLICATE_EMAIL'}, status=400)
-            
+                 return JsonResponse({'message': 'DUPLICATE_EMAIL'}, status=400)
+             
+             if not re.match(REGEX_LAST_NAME,last_name):
+                 return JsonResponse({'message' : 'INVALID_LAST_NAME'}, status = 400)
+             
+             if not re.match(REGEX_FIRST_NAME,first_name):
+                 return JsonResponse({'message' : 'INVALID_FIRST_NAME'}, status = 400)
+             
              User.objects.create(
-                first_name    = data['first_name'],
-                last_name     = data['last_name'],
-                email         = data['email'],
-                password      = data['password'],
-                phone_number  = data['phone_number'],
+                first_name   = data['first_name'],
+                last_name    = data['last_name'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
             )
              
              return JsonResponse({'message' : 'SUCCESS'}, status=201)
         except KeyError:
              return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+class SigninView(View):
+    def post(self, request):
+        try :
+             
+               User(
+                   user_id     = data['user_id'],
+                   email       = data['email'],
+                   password    = data['password'],
+               )
+
+               if User.objects.filter(user_id = data['user_id']).exists() == True:
+                   return JsonResponse({"message" : "이미 존재하는 아이디입니다."}, status = 401)
+       
+               else:
+                   User.objects.create(user_id = data['user_id'], email = data['email'], password = data['password'])
+                   
+             
+                   return JsonResponse({'message' : 'SUCCESS'}, status=201)
+        except KeyError:
+             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)         
